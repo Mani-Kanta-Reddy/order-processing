9b3bbf6fc6e5582277fb7237c8b158aa7b76a5ec
mummad499 <manikantareddy_ummadi@comcast.com>
2025-05-03 11:27:08 +0530

    initial commit

---
+++ src/test/java/com/orderprocessing/orderprocessing/OrderProcessingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.orderprocessing.orderprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
